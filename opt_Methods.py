ce4b96d9609fc504642067ad8f15cecae1cc9a2a
Ioannis Liaggos <ioannisliaggos@192.168.1.96>
2022-01-10 02:02:14 +0200

    Complete new approach for one truck

--- opt_Methods.py
+++ opt_Methods.py
@@ -30,8 +30,9 @@ customers = int(interesting_rows[4][1])
 
 # print(interesting_rows)
 df = pd.read_csv(file_name, skiprows=10, index_col=0)
-df['Distance from 0'] = df.apply(lambda row : distance.euclidean(depot_position, (row["x"], row["y"])), axis=1)
 df['Position'] = df.apply(lambda row: (row["x"], row["y"]), axis=1)
+df['Distance from 0'] = df.apply(lambda row : distance.euclidean(depot_position, (row["x"], row["y"])), axis=1)
+
 # df.drop(['x', 'y'], axis=1, inplace=True)
 # min_service_time = df.sort_values(by=["Service Time"], inplace=True)
 # print(df)
@@ -79,12 +80,12 @@ print(max_profit)
 # df.drop(['x', 'y'], axis=1, inplace=True)
 fltered_df = df[df['Distance from 0'] <= remaining_time]
 print(fltered_df)
-fltered_df.loc[0] = [depot_position[0], depot_position[1], 0, 0, 0, 0, depot_position]
+fltered_df.loc[0] = [depot_position[0], depot_position[1], 0, 0, 0, depot_position, 0]
 for node in fltered_df.index:
     if node != 0:
         position = fltered_df.loc[node]["Position"]
         fltered_df['Distance from ' + str(node)] = fltered_df.apply(lambda row : distance.euclidean(position, (row["x"], row["y"])), axis=1)
-# print(fltered_df)
+print(fltered_df)
 
 nodes = fltered_df.T.to_dict()
 print("Number of possible nodes")
@@ -98,71 +99,93 @@ print(len(nodes))
 w1 = 1
 w2 = 1
 w3 = 1
-w4 = 2
+w4 = 3
 solution_found = False
-
 solution = [0]
+solution_profit = 0
+remaining_time = max_duration
+remainig_capacity = max_capacity 
 while not solution_found:
     qualities = []
     for node in nodes.keys():
         if node not in solution:
-            print("Node = " + str(node))
+            # print("Node = " + str(node))
             # print(nodes.get(node))
             q1 = w1 * (1-nodes.get(node)["Demand"] / fltered_df["Demand"].max())
             q2 = w2 * (1-nodes.get(node)["Service Time"] / fltered_df["Service Time"].max())
-            q3 = w3 * (1-nodes.get(solution[-1])["Distance from " + str(node)] / fltered_df["Distance from " + str(node)].max())
+            columns = ["Distance from " + str(x)  for x in nodes.keys()]
+            q3 = w3 * (1-nodes.get(solution[-1])["Distance from " + str(node)] / fltered_df.loc[solution[-1]][columns].max())
             q4 = w4 * (nodes.get(node)["Profit"] / fltered_df["Profit"].max())
             quality = q1 + q2 + q3 + q4
             qualities.append([node, quality])
     qualities = sorted(qualities ,key=lambda x: x[1], reverse=True)
-    print(qualities)
-    paok
-
-
-paok
-combinations_df = pd.DataFrame(columns = ['Combination', 'Profit'])
-combinations_nodes = []
-for i in range(11, 10, -1):
-    for combination in list(combinations(nodes.keys(), i)):
-        capacity = sum(nodes.get(node)["Demand"] for node in combination)
-        service_time = sum(nodes.get(node)["Service Time"] for node in combination)
-        if capacity <= max_capacity and service_time <= max_duration:
-            profit = sum(nodes.get(node)["Profit"] for node in combination)
-            combinations_nodes.append([combination, profit])
-
-    
-combinations_nodes = sorted(combinations_nodes ,key=lambda x: x[1], reverse=True)
-print("Number of combinations")
-print(len(combinations_nodes))
-
-nodes[0] = {"Position": depot_position}
-# solutions = []
-dinstances = {}
-found_solution = False
-for row in combinations_nodes:
-    if not found_solution:
-        print("Combination length")
-        print(len(row[0]))
-        combination = row[0]
-        perm = list(permutations(combination))
-        # print(perm)
-        for permutation in perm:
-            if not found_solution:
-                permutation_distances = sum(nodes.get(tuple_value)["Service Time"] for tuple_value in permutation)
-                complete_permutation = (0,) + permutation + (0,)
-                for tuple_index in range(len(complete_permutation) - 1):
-                    dinstances_key = tuple(sorted(complete_permutation[tuple_index:(tuple_index + 2)]))
-                    # print(dinstances_key)
-                    dst = dinstances.get(dinstances_key)
-                    if dst == None:
-                        dinstances[dinstances_key] = dst = distance.euclidean(nodes.get(dinstances_key[0])["Position"], nodes.get(dinstances_key[1])["Position"])
-                    permutation_distances += dst
-
-                if permutation_distances <= max_duration:
-                    print("Solution found")
-                    print(row)
-                    found_solution = True
-                    # solutions.append([complete_permutation, sum(nodes.get(tuple_value)["Profit"] for tuple_value in permutation)])
-
+    local_solution_found = False
+    for quality in qualities:
+        if not local_solution_found:
+            node = quality[0]
+            if remaining_time - nodes.get(node)["Service Time"] - nodes.get(solution[-1])["Distance from " + str(node)] - nodes.get(node)["Distance from 0"] > 0 and remainig_capacity - nodes.get(node)["Demand"] > 0:
+                remaining_time = remaining_time - nodes.get(node)["Service Time"] - nodes.get(solution[-1])["Distance from " + str(node)]
+                remainig_capacity = remainig_capacity - nodes.get(node)["Demand"]
+                solution.append(node)
+                solution_profit = solution_profit + nodes.get(node)["Profit"]
+                local_solution_found = True
+    if local_solution_found == False:
+        solution_found = True
+        solution.append(0)
+    # print(qualities)
+print("----------Solution------------")
+print("Combination = " + str(solution))
+print("Total Profit = " + str(solution_profit))
+print("------------------------------")
 end_time = time.perf_counter()
 print(end_time - start_time)
+
+
+#Old approach
+
+# combinations_df = pd.DataFrame(columns = ['Combination', 'Profit'])
+# combinations_nodes = []
+# for i in range(11, 10, -1):
+#     for combination in list(combinations(nodes.keys(), i)):
+#         capacity = sum(nodes.get(node)["Demand"] for node in combination)
+#         service_time = sum(nodes.get(node)["Service Time"] for node in combination)
+#         if capacity <= max_capacity and service_time <= max_duration:
+#             profit = sum(nodes.get(node)["Profit"] for node in combination)
+#             combinations_nodes.append([combination, profit])
+
+    
+# combinations_nodes = sorted(combinations_nodes ,key=lambda x: x[1], reverse=True)
+# print("Number of combinations")
+# print(len(combinations_nodes))
+
+# nodes[0] = {"Position": depot_position}
+# # solutions = []
+# dinstances = {}
+# found_solution = False
+# for row in combinations_nodes:
+#     if not found_solution:
+#         print("Combination length")
+#         print(len(row[0]))
+#         combination = row[0]
+#         perm = list(permutations(combination))
+#         # print(perm)
+#         for permutation in perm:
+#             if not found_solution:
+#                 permutation_distances = sum(nodes.get(tuple_value)["Service Time"] for tuple_value in permutation)
+#                 complete_permutation = (0,) + permutation + (0,)
+#                 for tuple_index in range(len(complete_permutation) - 1):
+#                     dinstances_key = tuple(sorted(complete_permutation[tuple_index:(tuple_index + 2)]))
+#                     # print(dinstances_key)
+#                     dst = dinstances.get(dinstances_key)
+#                     if dst == None:
+#                         dinstances[dinstances_key] = dst = distance.euclidean(nodes.get(dinstances_key[0])["Position"], nodes.get(dinstances_key[1])["Position"])
+#                     permutation_distances += dst
+
+#                 if permutation_distances <= max_duration:
+#                     print("Solution found")
+#                     print(row)
+#                     found_solution = True
+#                     # solutions.append([complete_permutation, sum(nodes.get(tuple_value)["Profit"] for tuple_value in permutation)])
+
+# end_time = time.perf_counter()
+# print(end_time - start_time)
